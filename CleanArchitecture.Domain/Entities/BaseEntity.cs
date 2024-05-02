00cf2bba10514b76f5a6bc08ee6d0840234a9c44
Andrés Meóñez <joseanmeonez@gmail.com>
2024-05-01 20:53:10 -0600

    feat: audit fields

--- CleanArchitecture.Domain/Entities/BaseEntity.cs
+++ CleanArchitecture.Domain/Entities/BaseEntity.cs
@@ -3,4 +3,12 @@
 public abstract class BaseEntity<TId>
 {
 	public required TId Id { get; init; }
+
+	public string CreatedBy { get; init; } = null!;
+
+	public DateTime CreationDate { get; init; }
+
+	public string? UpdatedBy { get; set; }
+
+	public DateTime? UpdateDate { get; set; }
 }
