2336f862973573d944b234a43b1b037d0c36792c
Andrés Meoñez <76024612+JoseanMeonez@users.noreply.github.com>
2024-07-01 00:13:19 -0600

    fix: async method warning

--- CleanArchitecture.Application/Features/CustomerFeature/Queries/GetAll/GetAllCustomersQuery.cs
+++ CleanArchitecture.Application/Features/CustomerFeature/Queries/GetAll/GetAllCustomersQuery.cs
@@ -10,10 +10,6 @@ internal class GetAllCustomersQueryHandler : IRequestHandler<
 	GetAllCustomersQuery,
 	Response<BasicCustomerResponse>>
 {
-	public async Task<Response<BasicCustomerResponse>> Handle(
-		GetAllCustomersQuery request,
-		CancellationToken cancellationToken)
-	{
-		throw new KeyNotFoundException($"No se encontraron registros.");
-	}
+	public Task<Response<BasicCustomerResponse>> Handle(GetAllCustomersQuery request, CancellationToken cancellationToken)
+		=> throw new KeyNotFoundException($"No se encontraron registros.");
 }
